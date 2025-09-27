9e9db08a57b1e877b37913b87a7cd091ab24fe87
InaamCodes <inaammta@gmail.com>
2025-09-27 14:47:09 +0530

    Add bottom tab navigation with Home and Profile screens
    Introduced a BottomTabs navigator to replace the Home screen with a tabbed interface including Home and a new Profile screen. Updated navigation flow in App.js and LoginScreen.js to use the new tab navigator. Added ProfileScreen with basic user info and logout functionality.

--- my-app/frontend/App.js
+++ my-app/frontend/App.js
@@ -2,10 +2,11 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-// Import all screens from src/screens
-
-import HomeScreen from "./src/screens/HomeScreen";
+// Import stack screens
 import LoginScreen from "./src/screens/LoginScreen";
+
+// Import Bottom Tabs
+import BottomTabs from "./src/navigation/BottomTabs";
 
 const Stack = createNativeStackNavigator();
 
@@ -13,8 +14,19 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="Home" component={HomeScreen} />
+        {/* Login screen */}
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ headerShown: false }}
+        />
+
+        {/* Home replaced by Bottom Tabs */}
+        <Stack.Screen
+          name="HomeTabs"
+          component={BottomTabs}
+          options={{ headerShown: false }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
